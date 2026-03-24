12eb78c967a6b7af992a2c3f7894b130309b1b5e
nostackDotDev <developer.nostack@gmail.com>
2026-03-24 23:54:44 +0100

    Backend update
    * fix: Atualização dos fucheiros de configuração
    - Implementação do uso do Prisma para conexão e consultas à DB
    - Configuração de versionamento de pacotes e depenências estável
    
    feat: Criação de seeder para nserção de dados (de exemplo) à DB
    
    * fix: Atualização dos fucheiros de configuração
    - Implementação do uso do Prisma para conexão e consultas à DB
    - Configuração de versionamento de pacotes e depenências estável
    
    feat: Criação de seeder para nserção de dados (de exemplo) à DB
    
    ---------
    
    Co-authored-by: HJ-Developer

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,19 +8,9 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const port =  process.env.PORT || 8001;
+const prisma = require("../lib/prisma")
 
-const db = mysql.createConnection({
-  host: process.env.TIDB_HOST || "localhost",
-  port: process.env.TIDB_PORT || 4000,
-  user: process.env.TIDB_USER || "root",
-  password: process.env.TIDB_PASS || "",
-  database: process.env.TIDB_DB || "sgi",
-  ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  }
-});
+const port = process.env.PORT || 8001;
 
 app.get("/", (req, res) => {
   res.json({
@@ -32,53 +22,61 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/api/users", (req, res) => {
-  db.query("SELECT nome, password, nivel FROM users", (err, row) => {
-    if (err)
-      return res.status(500).json({
-        message: "An error occurred",
-        error: err.message,
-      });
-    if (row.length == 0)
-      return res.status(404).json({
-        message: "No match found",
-        data: [],
-      });
-    res.json({ message: "Fetched successfully", data: row });
-  });
-});
+app.get("/users", async (_, res) => {
+  const users = await prisma.user.findMany()
 
-app.post("/api/users/login", (req, res) => {
-  const { nome, password } = req.body;
+  res.json({
+    data: users
+  })
+})
 
-  if (!nome || !password) {
-    return res.status(400).json({
-      message: "Nome and password are required",
-    });
-  }
+// app.get("/api/users", (req, res) => {
+//   db.query("SELECT nome, password, nivel FROM users", (err, row) => {
+//     if (err)
+//       return res.status(500).json({
+//         message: "An error occurred",
+//         error: err.message,
+//       });
+//     if (row.length == 0)
+//       return res.status(404).json({
+//         message: "No match found",
+//         data: [],
+//       });
+//     res.json({ message: "Fetched successfully", data: row });
+//   });
+// });
 
-  db.query(
-    "SELECT nome, password, nivel FROM users WHERE nome=? AND password=?",
-    [nome, password],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({
-          message: "An error occurred",
-          error: err.message,
-        });
-      }
+// app.post("/api/users/login", (req, res) => {
+//   const { nome, password } = req.body;
 
-      if (result.length === 0) {
-        return res.status(404).json({
-          message: "No match found",
-          data: [],
-        });
-      }
+//   if (!nome || !password) {
+//     return res.status(400).json({
+//       message: "Nome and password are required",
+//     });
+//   }
 
-      res.json({ message: "Fetched successfully", data: result });
-    }
-  );
-});
+//   db.query(
+//     "SELECT nome, password, nivel FROM users WHERE nome=? AND password=?",
+//     [nome, password],
+//     (err, result) => {
+//       if (err) {
+//         return res.status(500).json({
+//           message: "An error occurred",
+//           error: err.message,
+//         });
+//       }
+
+//       if (result.length === 0) {
+//         return res.status(404).json({
+//           message: "No match found",
+//           data: [],
+//         });
+//       }
+
+//       res.json({ message: "Fetched successfully", data: result });
+//     }
+//   );
+// });
 
 app.listen(port, '0.0.0.0', () => {
   console.log("Listening on port", port);
