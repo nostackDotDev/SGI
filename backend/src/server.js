61fabeb9caab9e102edf267a360856cd0a9bca06
HJ-Developer <helvio.fr.jose@gmail.com>
2026-03-24 23:35:21 +0100

    fix: Atualização dos fucheiros de configuração - Implementação do uso do Prisma para conexão e consultas à DB - Configuração de versionamento de pacotes e depenências estável
    feat: Criação de seeder para nserção de dados (de exemplo) à DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,19 +8,20 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const port =  process.env.PORT || 8001;
+// const port =  process.env.PORT || 8001;
+const port = 3001
 
-const db = mysql.createConnection({
-  host: process.env.TIDB_HOST || "localhost",
-  port: process.env.TIDB_PORT || 4000,
-  user: process.env.TIDB_USER || "root",
-  password: process.env.TIDB_PASS || "",
-  database: process.env.TIDB_DB || "sgi",
-  ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  }
-});
+// const db = mysql.createConnection({
+//   host: process.env.TIDB_HOST || "localhost",
+//   port: process.env.TIDB_PORT || 4000,
+//   user: process.env.TIDB_USER || "root",
+//   password: process.env.TIDB_PASS || "",
+//   database: process.env.TIDB_DB || "sgi",
+//   ssl: {
+//     minVersion: 'TLSv1.2',
+//     rejectUnauthorized: true
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.json({
@@ -32,53 +33,53 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/api/users", (req, res) => {
-  db.query("SELECT nome, password, nivel FROM users", (err, row) => {
-    if (err)
-      return res.status(500).json({
-        message: "An error occurred",
-        error: err.message,
-      });
-    if (row.length == 0)
-      return res.status(404).json({
-        message: "No match found",
-        data: [],
-      });
-    res.json({ message: "Fetched successfully", data: row });
-  });
-});
+// app.get("/api/users", (req, res) => {
+//   db.query("SELECT nome, password, nivel FROM users", (err, row) => {
+//     if (err)
+//       return res.status(500).json({
+//         message: "An error occurred",
+//         error: err.message,
+//       });
+//     if (row.length == 0)
+//       return res.status(404).json({
+//         message: "No match found",
+//         data: [],
+//       });
+//     res.json({ message: "Fetched successfully", data: row });
+//   });
+// });
 
-app.post("/api/users/login", (req, res) => {
-  const { nome, password } = req.body;
+// app.post("/api/users/login", (req, res) => {
+//   const { nome, password } = req.body;
 
-  if (!nome || !password) {
-    return res.status(400).json({
-      message: "Nome and password are required",
-    });
-  }
+//   if (!nome || !password) {
+//     return res.status(400).json({
+//       message: "Nome and password are required",
+//     });
+//   }
 
-  db.query(
-    "SELECT nome, password, nivel FROM users WHERE nome=? AND password=?",
-    [nome, password],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({
-          message: "An error occurred",
-          error: err.message,
-        });
-      }
+//   db.query(
+//     "SELECT nome, password, nivel FROM users WHERE nome=? AND password=?",
+//     [nome, password],
+//     (err, result) => {
+//       if (err) {
+//         return res.status(500).json({
+//           message: "An error occurred",
+//           error: err.message,
+//         });
+//       }
 
-      if (result.length === 0) {
-        return res.status(404).json({
-          message: "No match found",
-          data: [],
-        });
-      }
+//       if (result.length === 0) {
+//         return res.status(404).json({
+//           message: "No match found",
+//           data: [],
+//         });
+//       }
 
-      res.json({ message: "Fetched successfully", data: result });
-    }
-  );
-});
+//       res.json({ message: "Fetched successfully", data: result });
+//     }
+//   );
+// });
 
 app.listen(port, '0.0.0.0', () => {
   console.log("Listening on port", port);
