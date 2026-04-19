3f5a9c9184f488ce48663f37e6ec15234123d2ea
HJ-Developer <helvio.fr.jose@gmail.com>
2026-04-19 09:24:40 +0100

    feat: endpoint para vadastro de utilizador feito

--- backend/src/routes/public.js
+++ backend/src/routes/public.js
@@ -28,21 +28,41 @@ router.get("/users", async (_, res) => {
 
 router.post("/cadastro", async (req, res) => {
   try {
-    const { email, nome, password } = req.body
-    await prisma.utilizador.create({
+    const { email, nome, password, cargoId } = req.body;
+
+    if (!password) {
+      return res.status(400).json({ error: "Password is required" });
+    }
+
+    const cargo = await prisma.cargo.findUnique({
+      where: { id: cargoId },
+    });
+
+    if (!cargo) {
+      return res.status(404).json({
+        message: "Falha ao criar utilizador",
+        error: "Cargo não encontrado",
+      });
+    }
+
+    const user = await prisma.utilizador.create({
       data: {
         email,
         nome,
         password,
-      }
-    })
-    res.status(201).json(user)
+        cargoId, // ✅ fixed
+        descricao: "",
+      },
+    });
+
+    res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({ message: "Erro found, try again", error: error.message })
+    res.status(500).json({
+      message: "Erro found, try again",
+      error: error.message,
+    });
   }
-
-
-})
+});
 
 
 export default router
