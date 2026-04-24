ab17535960f76e2ea2b8b029e56ae169700f7752
HJ-Developer <helvio.fr.jose@gmail.com>
2026-04-25 00:24:29 +0100

    feat: Atualização do middleware de autenticação para suportar a recuperação de tokens a partir de cookies e adição da funcionalidade de logout.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,10 +3,29 @@ import {
   loginController,
   signupController,
 } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({
+    user: req.user,
+  });
+});
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  return res.json({
+    message: "Logged out successfully",
+  });
+});
 
 export default router;
